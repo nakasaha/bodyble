71b801c49f9a09fb14269239c72393abbdba856a
なかさは <nyantamikaanko39@gmail.com>
2025-06-30 12:43:20 +0900

    アプリケーションファイルの変更

--- src/main/java/com/system/management/weight/WmsApplication.java
+++ src/main/java/com/system/management/weight/WmsApplication.java
@@ -2,12 +2,24 @@ package com.system.management.weight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WmsApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String getLogin(Model model) {
+		/*
+		 * ログイン画面へ遷移
+		 */
+		return "login";
 	}
 
 }
